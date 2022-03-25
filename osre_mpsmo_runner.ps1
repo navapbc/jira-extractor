b450ca31ef7757152e9adc5ca36a3892a14dffc7
j c <jimmycarter@navapbc.com>
2022-03-25 15:50:32 +0000

    Update osre_mpsmo_runner.ps1
    added python init to ps1 script

--- osre_mpsmo_runner.ps1
+++ osre_mpsmo_runner.ps1
@@ -2,4 +2,7 @@ $Env:JIRA_URL = 'https://jiraent.cms.gov'
 $Env:JIRA_USER = '<account>'
 $Env:JIRA_PASSWORD = '<pw>'
 $Env:JIRA_PROJECT = 'MPSMO'
+python -m venv venv
+.\venv\Scripts\activate.ps1
+python .\venv\Scripts\pip.exe install atlassian-python-api python-dateutil
 python .\jira-extractor.py
